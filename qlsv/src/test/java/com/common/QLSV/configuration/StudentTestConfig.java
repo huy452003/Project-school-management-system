233217e90810b341313b82a1e7b7dfd9e13030f7
huy452003 <tranhuyaz937@gmail.com>
2025-12-29 15:10:30 +0700

    update deploy

---
+++ qlsv/src/test/java/com/common/QLSV/configuration/StudentTestConfig.java
@@ -0,0 +1,13 @@
+package com.common.QLSV.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+/**
+ * Test configuration class for Student tests.
+ * This is a placeholder to avoid compilation errors.
+ */
+@TestConfiguration
+public class StudentTestConfig {
+    // Empty configuration class for testing purposes
+}
+
